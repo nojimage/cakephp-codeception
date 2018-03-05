ec592e21102bac844c81d28f8dc3343ae2d2738e
nojimage <nojimage@gmail.com>
2018-03-05 12:10:07 +0900

    Support fixture in cept
    backport from elstc/codeception-cakefixture

--- tests/sample_app/tests/Functional/LoadFixturesCest.php
+++ tests/sample_app/tests/Functional/LoadFixturesCest.php
@@ -5,15 +5,16 @@ namespace App\Test\Functional;
 use App\TestSuite\Codeception\FunctionalTester;
 
 /**
- * Test for loadFixtures()
+ * Test for CakeFixture::loadFixtures()
  */
-class FixtureLoadCest
+class LoadFixturesCest
 {
 
     public $autoFixtures = false;
+
     public $fixtures = [
         'core.authors',
-        'core.posts',
+        'core.posts'
     ];
 
     /**
@@ -21,12 +22,15 @@ class FixtureLoadCest
      */
     public function tryLoadFixtures(FunctionalTester $I)
     {
-        $I->wantTo('loading fixtures manulary');
         $I->loadFixtures('Authors');
         $I->seeInDatabase('authors', ['id' => 1, 'name' => 'mariano']);
 
         $I->loadFixtures();
         $I->seeInDatabase('posts', ['author_id' => 1, 'title' => 'First Post']);
-    }
 
+        // useFixtures use only once
+        $I->expectException('\Codeception\Exception\ModuleException', function () use ($I) {
+            $I->useFixtures('core.tags');
+        });
+    }
 }
